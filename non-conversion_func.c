54e08eee1ec8a5eedd2ca7dd33cfc15a57a80186
SollyTlax <sollytlaks@gmail.com>
2023-08-20 11:55:32 -0700

    handle specifiers

--- non-conversion_func.c
+++ non-conversion_func.c
@@ -106,3 +106,39 @@ int handle_precision(const char *format, int *i, va_list args)
 	*i = curr_index - 1;
 
 	return (precision);
+}
+
+/**
+ * handle_flags - extract and process formatting flags from a string used in
+ * output function
+ * @format: a pointer to a format string
+ * @i: current position in the format string
+ *
+ * Return: Flags
+ */
+int handle_flags(const char *format, int *i)
+{
+
+	int j, curr_index;
+	int flags = 0;
+	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
+	const int FLAGS_ARR[] = {FLAG_MINUS, FLAG_PLUS, FLAG_ZERO,
+		 FLAG_HASH, FLAG_SPACE, 0};
+
+	for (curr_index = *i + 1; format[curr_index] != '\0'; curr_index++)
+	{
+		for (j = 0; FLAGS_CH[j] != '\0'; j++)
+			if (format[curr_index] == FLAGS_CH[j])
+			{
+				flags |= FLAGS_ARR[j];
+				break;
+			}
+
+		if (FLAGS_CH[j] == 0)
+			break;
+	}
+
+	*i = curr_index - 1;
+
+	return (flags);
+}
